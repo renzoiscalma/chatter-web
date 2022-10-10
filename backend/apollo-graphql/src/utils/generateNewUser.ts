d4c2c22c0127e33dd3dec61474b65ee08b032b9f
Renzo Calma <xeganthy@gmail.com>
2022-10-10 20:21:20 -0300

    Added random name generator for users

---
+++ backend/apollo-graphql/src/utils/generateNewUser.ts
@@ -0,0 +1,14 @@
+import { uniqueNamesGenerator, Config, adjectives, colors, animals } from "unique-names-generator"
+
+export const generateNewUser = (seed: string | number): string => {
+	console.log(seed);
+	const generatorConfig: Config = {
+		dictionaries: [adjectives, colors, animals],
+		length: 3, 
+		style: "capital",
+		separator: " ",
+		seed
+	}
+
+	return uniqueNamesGenerator(generatorConfig);
+}
