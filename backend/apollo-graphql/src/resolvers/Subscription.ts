b2d05730bb6a4b637e304d32a886d72bf46500af
Renzo Calma <xeganthy@gmail.com>
2022-10-12 11:14:41 -0300

    Removed unecessary console logs

--- backend/apollo-graphql/src/resolvers/Subscription.ts
+++ backend/apollo-graphql/src/resolvers/Subscription.ts
@@ -7,12 +7,10 @@ const subResolver = {
 	Subscription: {
 		messageAdded: { // todo types
 			subscribe: withFilter(
-				(_: any, args: any) => {
+				() => {
 					return pubsub.asyncIterator(MESSAGE_ADDED_TOPIC);
 				},
 				(payload: any, variables: any) => {
-					console.log("hey", payload.messageAdded, "zzz", payload.messageAdded.messages[0].from.id);
-					console.log(payload.lobbyId, variables, typeof(payload.lobbyId), typeof(variables.lobbyId));
 					return payload.messageAdded.lobbyId === variables.lobbyId && variables.userId !== payload.messageAdded.messages[0].from.id;
 				}
 			)
