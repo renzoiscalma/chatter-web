47b68d071cf9df9e8a8c09073032e2b3e66ebd87
Renzo Calma <xeganthy@gmail.com>
2022-11-09 19:54:42 -0400

    Added dark mode toggle & context change handler

--- frontend/react/chatter-react/src/components/Chatter/interface/UserContext.tsx
+++ frontend/react/chatter-react/src/components/Chatter/interface/UserContext.tsx
@@ -2,6 +2,8 @@ interface UserContext {
   username: string;
   userId: string;
   lobbyId: string;
+  darkMode: boolean;
   setUsername(username: String): void;
+  darkModeToggle(): void;
 }
 export default UserContext;
