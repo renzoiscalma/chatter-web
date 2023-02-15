497bbe3430bcb546022bf90493916070d261c26e
Renzo Calma <xeganthy@gmail.com>
2023-02-15 19:36:23 -0400

    Added SimpleModal validation params

--- frontend/react/chatter-react/src/util/helpers.ts
+++ frontend/react/chatter-react/src/util/helpers.ts
@@ -1,7 +1,11 @@
 const validateYtUrl = (url: string): boolean => {
-  var p =
+  let p =
     /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
   return url.match(p) ? true : false;
 };
 
-export { validateYtUrl };
+const validateUsername = (username: string): boolean => {
+  return username.trim().length <= 36 && username.trim().length >= 3;
+};
+
+export { validateYtUrl, validateUsername };
