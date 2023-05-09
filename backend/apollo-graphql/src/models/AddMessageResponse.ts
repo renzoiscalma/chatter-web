22833a229a9b7803093d1d4f0fa0b04ba3602e92
Renzo Calma <xeganthy@gmail.com>
2023-05-09 19:18:21 -0300

    Deleted backend folder in preparation for submodule

--- backend/apollo-graphql/src/models/AddMessageResponse.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from "./Message";
-
-export interface AddMessageResponse {
-  code: number;
-  success: boolean;
-  message?: Message;
-  localDateSent: string;
-}
--- backend/apollo-graphql/src/utils/const.ts
+++
@@ -1,4 +0,0 @@
-export const MESSAGE_ADDED_TOPIC = "messageAddedTopic";
-export const VIDEO_STATUS_TOPIC = "videoStatusTopic";
-export const USERNAME_CHANGED_TOPIC = "usernameChangedTopic";
-export const USER_LIST_UPDATED_TOPIC = "userListUpdatedTopic";
